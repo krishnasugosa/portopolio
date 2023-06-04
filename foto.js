443abd0446b23958ad49527bb76b2fa69813f54a
code.shreek <127308616+krishnasugosa@users.noreply.github.com>
2023-06-04 21:10:46 +0800

    Add files via upload

---
+++ foto.js
@@ -0,0 +1,18 @@
+let body = document.body;
+
+function purple() {
+    body.classList.toggle("ganti")
+}
+
+document.getElementById("yt").onclick = function () {
+    window.location = "https://www.youtube.com/@code.shreek"
+}
+
+document.getElementById("gm").onclick = function () {
+    window.location = "https://krishnasugosa.github.io/whatsapp-jump/"
+}
+
+document.getElementById("gi").onclick = function () {
+    window.location = "https://github.com/krishnasugosa?tab=repositories"
+}
+ 
